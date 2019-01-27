eedccc5c46da4d114979cd3fc77ddfb7f36c7d09
johnzhuggcs <johnzhu73@yahoo.com>
2019-01-27 04:23:48 -0800

    finished maps sorta

---
+++ app/src/main/java/com/example/bonfire/WaitingScreen.java
@@ -0,0 +1,13 @@
+package com.example.bonfire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaitingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_screen);
+    }
+}
